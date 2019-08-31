3b581d3a1f0e1cc628a954643f28bbb57c825ea5
saadi hamza <saadi.sidali@outlook.fr>
2019-08-31 14:30:07 +0200

    test from vs code

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+  CategoryItem(this.title, this.color);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      color: color,
+      child: Text(title),
+    );
   }
 }
